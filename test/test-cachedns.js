617bd3006ae0503a319bc2ae2a30f799c2f003e7
Trevor Livingston <trlivingston@paypal.com>
2015-11-10 19:48:22 -0600

    make the key more unique to function and arguments

--- test/test-cachedns.js
+++ test/test-cachedns.js
@@ -3,24 +3,35 @@
 import Test from 'tape';
 import cachedns from '../lib';
 
-Test.only('cachedns', function (t) {
+Test.only('cachedns', (t) => {
 
     let dns = cachedns();
 
-    t.test('lookup', function (t) {
+    t.test('lookup', (t) => {
         t.plan(7);
 
         dns.lookup('google.com', (error, addresses, family) => {
             t.ok(!error, 'no error.');
             t.ok(addresses, 'found results.');
             t.ok(family, 'family found.');
-            t.ok(dns._cache.search('google.com'), 'in cache.');
+            t.ok(dns._cache.search('lookup_google.com'), 'in cache.');
 
             dns.lookup('google.com', (error, addresses, family) => {
                 t.ok(!error, 'no error.');
                 t.ok(addresses, 'found results.');
                 t.ok(family, 'family found.');
             });
+        });
+    });
+
+    t.test('options', (t) => {
+        t.plan(4);
+
+        dns.lookup('google.com', {family: 4}, (error, addresses, family) => {
+            t.ok(!error, 'no error.');
+            t.ok(addresses, 'found results.');
+            t.ok(family, 'family found.');
+            t.ok(dns._cache.search('lookup_google.com_family_4'), 'in cache.');
         });
     });
 
